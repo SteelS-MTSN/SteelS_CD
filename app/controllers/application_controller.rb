8a79f3cf26c935833acc19007841b6408b7b1171
obanozin <obanozin18@gmail.com>
2019-09-15 09:49:16 +0000

    almost finish devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,25 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+  	case resource
+  	when User
+    	user_path(current_user)
+    when Admin
+    	admins_path
+    end
   end
 def after_sign_out_path_for(resource)
-	new_user_session_path
+	if resource.is_a?(User)
+		new_user_session_path
+	else
+		new_admin_session_path
+	end
+	# case resource
+	# when User
+	# new_user_session_path
+	# when Admin
+	# 	new_admin_session_path
+	# end
 end
   protected
   def configure_permitted_parameters
