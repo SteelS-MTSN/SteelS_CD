b725e76ebb18e0213a01aae3e988edbf496ce69d
obanozin <obanozin18@gmail.com>
2019-09-16 09:13:32 +0000

    totyu

--- app/controllers/admins_users_controller.rb
+++ app/controllers/admins_users_controller.rb
@@ -11,4 +11,16 @@ class AdminsUsersController < ApplicationController
 		@user = User.find(params[:id])
 		
 	end
+
+	def edit
+		@user = User.find(params[:id])
+		
+	end
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+ 
+      
+    
+  end
 end
