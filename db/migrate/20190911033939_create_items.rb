6cdc589523dbdbbd8a691c28a38c73996fda9216
tarabafussy <sasabinfussy@gmail.com>
2019-09-19 13:17:16 +0900

    newitem create

--- db/migrate/20190911033939_create_items.rb
+++ db/migrate/20190911033939_create_items.rb
@@ -6,12 +6,8 @@ class CreateItems < ActiveRecord::Migration[5.2]
     	t.integer :price, null: false
     	t.integer :genre_id, null: false
     	t.integer :label_id, null: false
-<<<<<<< HEAD
     	t.integer :items_status, null: false, default: 0
     	t.boolean :is_deleted, null: false, default: false
-    	t.integer :artist_id, null: false
-    	t.integer :genre_id, null: false
-    	t.integer :label_id, null: false
         t.string :item_image_id
       t.timestamps
     end
