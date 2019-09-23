8162bf116eb697edb7f18e96045e1aca3c3ce21a
obanozin <obanozin18@gmail.com>
2019-09-23 10:32:11 +0000

    item_index_almost_finish

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,33 +1,23 @@
 class ItemsController < ApplicationController
-	Per = 9
+	PER = 9
 	def index
 
-
+   	    
+        @search = Item.ransack(params[:q])
+        @results = @search.result
+        @item = @results.page(params[:page]).per(PER)
 		
-		# 検索
-		if params[:keyword].present?
-			keyword = params[:keyword]
-			@result = ::Item.joins(:artist).all
-			like_keyword = "%#{keyword}%"
-			@result = @result.where('items.item_name like ?',like_keyword).
-			or(@result.where('artists.artist_name like ?',like_keyword)).order('items.item_name asc')
-			@items = @result
+
 			# @all_ranks = Item.find(1).artist.artist_name
 			# binding.pry
 			@allitems = Item.limit(5).order('id DESC')
 
-		else
-	 		@items = Item.all
-	 		@items = Item.page(params[:page]).per(Per)
-
-
-
-	 		@allitems = Item.limit(5).order('id DESC')
+			@allitems = Item.limit(5).order('id DESC')
 	 		@item_favorites_count = Item.joins(:favorites).group(:id).count
 	 		@item_favorites_ids = Hash[@item_favorites_count.sort_by{|_, v| -v }].keys
 	 		@item_side = Item.where(id: @item_favorites_ids).page(params[:page]).per(9)
 
-	 	end
+
 	 	# #違うテーブルで検索
 	 	# @items = @search.result(distinct: true).includes(:artist).joins(:artist).page(params[:page]).per(6)
 	 	# # 検索結果
