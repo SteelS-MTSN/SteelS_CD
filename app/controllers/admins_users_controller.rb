612a7579fc6b1afccf6681b38ebc6ee9ec1b3dfb
tarabafussy <sasabinfussy@gmail.com>
2019-09-25 18:05:09 +0900

    enumhelper install

--- app/controllers/admins_users_controller.rb
+++ app/controllers/admins_users_controller.rb
@@ -3,31 +3,25 @@ class AdminsUsersController < ApplicationController
 PER = 8
 	def index
         @users = User.page(params[:page]).reverse_order.per(PER)
-
-		
 	end
 
 	def show
 
 		@user = User.find(params[:id])
-		
 	end
 
 	def edit
 		@user = User.find(params[:id])
-		
 	end
-  def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
+	
+	def update
+	    @user = User.find(params[:id])
+	    @user.update(user_params)
+	    redirect_to admins_user_path(@user.id)
+	end
 
-    redirect_to admins_user_path(@user.id)
- 
-      
-    
-  end
-private 
-def user_params
-    params.require(:user).permit(:family_name, :first_name, :family_kana, :first_kana, :post_code, :address, :phone, :email)
-end
+	private
+	def user_params
+	    params.require(:user).permit(:family_name, :first_name, :family_kana, :first_kana, :post_code, :address, :phone, :email)
+	end
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -95,4 +95,3 @@ class CartsController < ApplicationController
 		end
 
 	end
-
